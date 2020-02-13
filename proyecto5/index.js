b7ce070473be9a4e6cae33c49552f47fb2325575
Daniel Mendoza <arceus6666@hotmail.com>
2020-02-12 22:10:20 -0400

    commit

---
+++ proyecto5/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const app = express();
+
+const server = http.createServer(app);
+
+const publicPath = path.resolve(__dirname, './public');
+
+app.use(express.static(publicPath));
+
+const io = socketIO(server);
+
+io.on('connection', (client) => {
+  // console.log(client);
+  console.log('Usuario conectado');
+  client.on('disconnect', () => {
+    console.log('Usuario desconectado');
+  });
+});
+
+
+
+server.listen(8010, () => {
+  console.log('Escuchando el puerto 8010');
+});
